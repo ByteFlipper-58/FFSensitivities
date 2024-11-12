4e299a51945540d4ae5ac4fd929a20c05a3f151c
Ibragim Maltsagov <byteflipper.business@gmail.com>
2024-11-12 22:56:54 +0100

    Improve opening links in Chrome Custom Tabs
    - Added checks to ensure a browser is available before attempting to open the link.
    - Improved the logic to set the toolbar color.
    - Added support for hiding the URL bar and displaying the website title.
    - Added a referrer to the intent to improve analytics and tracking.
    - Added a message for the user if a browser is not available.

--- app/src/main/java/com/byteflipper/ffsensitivities/utils/ChromeCustomTabUtil.java
+++ app/src/main/java/com/byteflipper/ffsensitivities/utils/ChromeCustomTabUtil.java
@@ -1,7 +1,10 @@
 package com.byteflipper.ffsensitivities.utils;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.widget.Toast;
 
 import androidx.browser.customtabs.CustomTabColorSchemeParams;
 import androidx.browser.customtabs.CustomTabsIntent;
@@ -10,8 +13,8 @@ public class ChromeCustomTabUtil {
 
     public static class Builder {
 
-        private Context context;
-        private String url;
+        private final Context context;
+        private final String url;
         private int toolbarColor;
 
         public Builder(Context context, String url) {
@@ -25,15 +28,25 @@ public class ChromeCustomTabUtil {
         }
 
         public void open() {
-            CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
-            CustomTabsIntent customTabsIntent = builder.build();
-            customTabsIntent.launchUrl(context, Uri.parse(url));
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            if (intent.resolveActivity(context.getPackageManager()) != null) {
+                CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
 
-            // Set tab color
-            CustomTabColorSchemeParams defaultColors = new CustomTabColorSchemeParams.Builder()
-                    .setToolbarColor(toolbarColor)
-                    .build();
-            builder.setDefaultColorSchemeParams(defaultColors);
+                CustomTabColorSchemeParams defaultColors = new CustomTabColorSchemeParams.Builder()
+                        .setToolbarColor(toolbarColor)
+                        .build();
+                builder.setDefaultColorSchemeParams(defaultColors);
+                builder.setUrlBarHidingEnabled(true);
+                builder.setShowTitle(true);
+                builder.build();
+
+                CustomTabsIntent customTabsIntent = builder.build();
+                customTabsIntent.launchUrl(context, Uri.parse(url));
+                customTabsIntent.intent.putExtra(Intent.EXTRA_REFERRER,
+                        Uri.parse("android-app://" + context.getPackageName()));
+            } else {
+                Toast.makeText(context, "No browser available to open link", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
